da67324efc1d77140e31678286cff447bf8d420b
Mathieu <creepymat44@gmail.com>
2023-03-20 12:40:47 +0100

    slider finished

--- src/js/ItemSlider.js
+++ src/js/ItemSlider.js
@@ -5,18 +5,50 @@ class ItemSlider extends React.Component {
     constructor(props) {
         super(props)
         this.sliderRef = React.createRef()
+        this.clicked = false
+        this.startX = null
     }
 
     componentDidUpdate(prevProps) {
         if (prevProps.currentItem !== this.props.currentItem) {
-            const itemWidth = 59 + 6; // Largeur d'un élément + espacement
+            const itemWidth = 59 + 6;
             const sliderWidth = this.sliderRef.current.clientWidth;
             const centerOffset = (sliderWidth - itemWidth) / 2;
             const scrollLeft = itemWidth * this.props.currentItem - centerOffset;
-            this.sliderRef.current.style.left = scrollLeft;
-            console.log(this.sliderRef.current.style)  
+            this.sliderRef.current.scrollLeft = scrollLeft;
         }
     }
+
+    handleMouseDown = (event) => {
+        this.clicked = true
+        this.sliderRef.current.style.scrollBehavior = "unset"
+        this.startX = event.clientX
+    }
+
+    handleMouseMove = (event) => {
+        if (this.clicked){
+            let scrollLeft = this.sliderRef.current.scrollLeft
+            scrollLeft -= event.movementX
+            this.sliderRef.current.scrollLeft = scrollLeft
+        }
+    }
+
+    handleMouseUp = (event) => {
+        this.clicked = false
+        this.sliderRef.current.style.scrollBehavior = "smooth"
+        if (this.startX && Math.abs(event.clientX - this.startX) > 5) {
+            this.startX = null;
+        } else {
+            this.props.onClick(event)
+        }
+    }
+
+    handleMouseLeave = () => {
+        this.clicked = false
+        this.sliderRef.current.style.scrollBehavior = "smooth"
+        this.startX = null
+    }
+
 
     render() {
         let itimgs = []
@@ -25,15 +57,25 @@ class ItemSlider extends React.Component {
             if (index === this.props.currentItem) {
                 classNames.push("currentItem")
             }
-            itimgs.push(<img key={item.id} alt={item.id} title={item.name} className={classNames.join(" ")} src={item.image} onClick={this.props.onClick}/>)
+            itimgs.push(<img 
+                key={item.id} 
+                alt={item.id} 
+                title={item.name} 
+                className={classNames.join(" ")} 
+                src={item.image} 
+                draggable={false}/>)
         });
 
         return (
             <div id="slider">
-                <div id="sliderinside" ref={this.sliderRef}>
+                <div id="sliderinside" 
+                ref={this.sliderRef} 
+                onMouseDown={this.handleMouseDown} 
+                onMouseUp={this.handleMouseUp} 
+                onMouseMove={this.handleMouseMove}
+                onMouseLeave={this.handleMouseLeave}>
                     {itimgs}
                 </div>
-                
             </div>
         )
     }
